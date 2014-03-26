83cc13898345ec78f2997b4bda99cc7123d28d16
m.otake <o.otake@os.infocom.co.jp>
2014-03-26 21:30:42 +0900

    MPOのラベルをグラフに表示するよう変更

--- mpo_genus_stanza/stanza.rb
+++ mpo_genus_stanza/stanza.rb
@@ -1,5 +1,18 @@
 class MpoGenusStanza < TogoStanza::Stanza::Base
   SPARQL_ENDPOINT_URL = 'http://ep.dbcls.jp/sparql71dev';
+	property :general do |mpo_id|
+		result = query(SPARQL_ENDPOINT_URL, <<-SPARQL.strip_heredoc)
+			PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+			PREFIX mpo:  <http://purl.jp/bio/01/mpo#>
+			select ?label
+			where {
+				mpo:#{mpo_id} rdfs:label ?label. 
+				filter( lang(?label) != "ja" )
+			}
+		SPARQL
+		
+		(result.first == nil) ? [] : result.first
+	end
   property :features do |mpo_id|
     query = <<-SPARQL.strip_heredoc
 	PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -20,6 +33,7 @@ class MpoGenusStanza < TogoStanza::Stanza::Base
 			?subject rdfs:subClassOf* ?list2 .
 			?list2 taxonomy:rank taxonomy:Genus .
 			?list2 rdfs:label ?genus
+			filter( lang(?genus) != "ja" )
 		}
 	}
 	group by ?genus ?list2
