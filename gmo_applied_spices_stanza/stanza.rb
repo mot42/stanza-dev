ba86d76f30fd082271604ebc29d70610feb3982f
m.otake <o.otake@os.infocom.co.jp>
2014-03-26 16:39:26 +0900

    引数を変更 med_id → medium_id

--- gmo_applied_spices_stanza/stanza.rb
+++ gmo_applied_spices_stanza/stanza.rb
@@ -1,11 +1,11 @@
 class GmoAppliedSpicesStanza < TogoStanza::Stanza::Base
-	property :applied_spices_list do |med_id|
+	property :applied_spices_list do |medium_id|
 		medium_list = query("http://ep.dbcls.jp/sparql71dev", <<-SPARQL.strip_heredoc)
 			PREFIX mccv: <http://purl.jp/bio/01/mccv#>
 			PREFIX gmo: <http://purl.jp/bio/11/gmo#>
 			select ?gmo_title ?label ?tax ?taxonomy_id
 			where {
-				?gmo gmo:GMO_000101 "#{med_id}" . 
+				?gmo gmo:GMO_000101 "#{medium_id}" . 
 				?gmo gmo:GMO_000102 ?gmo_title . 
 				?brc mccv:MCCV_000018 ?gmo .
 				?brc mccv:MCCV_000056 ?tax .
@@ -17,12 +17,12 @@ class GmoAppliedSpicesStanza < TogoStanza::Stanza::Base
 			order by ?label
 		SPARQL
 	end
-	property :general_information do |med_id|
+	property :general_information do |medium_id|
 		medium_list = query("http://ep.dbcls.jp/sparql71dev", <<-SPARQL.strip_heredoc)
 			PREFIX gmo: <http://purl.jp/bio/11/gmo#>
 			select ?gmo_title
 			where {
-				?gmo gmo:GMO_000101 "#{med_id}" . 
+				?gmo gmo:GMO_000101 "#{medium_id}" . 
 				?gmo gmo:GMO_000102 ?gmo_title . 
 			}
 		SPARQL
